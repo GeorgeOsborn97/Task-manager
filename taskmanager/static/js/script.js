cb4acad1c7699c92c9c0cd2e05c4eab47311ffb8
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-01-09 15:27:11 +0000

    next lesson

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      i18n: {done: 'Select'}
+    });
+    $('select').formSelect();
+    $('.collapsible').collapsible();
   });
+
+  /*$(document).ready(function(){
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy"
+    });
+  });*/
+            
