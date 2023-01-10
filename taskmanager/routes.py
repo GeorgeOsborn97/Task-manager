4f3778f51c0a6d4ce847785188804be0daeac374
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-01-10 09:14:47 +0000

    edit and delete

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -35,12 +35,30 @@ def edit_category(category_id):
     return render_template("edit_category.html", category=category)
 
 
+@app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if request.method == 'POST':
+        task.task_name = request.form.get('task_name')
+        db.session.commit()
+        return redirect(url_for('tasks'))
+    return render_template("edit_task.html", task=task)
+
+
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for('categories'))
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('tasks'))
 
 
 @app.route('/add_task', methods=['GET', 'POST'])
